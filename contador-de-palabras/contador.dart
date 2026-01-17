2d430cba2a14d5af9933c98c79cc00b0e93698c2
padredemellis <10656@holbertonstudents.com>
2026-01-16 21:42:26 -0300

    gestionar-claves-valores

---
+++ contador-de-palabras/contador.dart
@@ -0,0 +1,15 @@
+void main() {
+  String texto = "hola mundo hola dart mundo mundo";
+  
+  // TODO:  Divide el texto en palabras
+  List listaPalabras = [texto];
+  
+  
+  // TODO: Crea un mapa para contar la frecuencia de cada palabra
+  // Resultado esperado: {'hola': 2, 'mundo': 3, 'dart': 1}
+  
+  // TODO:  Encuentra la palabra más frecuente
+  
+  // TODO: Imprime palabras que aparecen más de 1 vez
+}
+```
