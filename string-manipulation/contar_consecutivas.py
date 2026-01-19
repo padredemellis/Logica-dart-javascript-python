f51703cff274d8e256fd81d5e0d0b444e7a34c0b
padredemellis <10656@holbertonstudents.com>
2026-01-19 19:43:39 -0300

    logica-string

--- string-manipulation/contar_consecutivas.py
+++ string-manipulation/contar_consecutivas.py
@@ -2,10 +2,26 @@
  Cuenta cuántas veces aparece un caracter de forma consecutiva al inicio
     Ejemplo: contar_consecutivos("aaabbc", "a") -> 3
 """
-def contar_consecutivos(texto, caracter):
+
+
+def contar_consecutivos(texto: str, caracter: str) -> int:
     """
     Cuenta cuántas veces aparece un caracter de forma consecutiva al inicio
     Ejemplo: contar_consecutivos("aaabbc", "a") -> 3
     """
-    # TU CÓDIGO AQUÍ
-    pass
+    if texto == "":
+        return 0
+    contador: int = 0
+    for c in texto:
+        if caracter.lower() == c.lower():
+            contador += 1
+        else:
+            break
+    return contador
+
+
+print(contar_consecutivos("AAAAAb", "a"))
+print(contar_consecutivos("AAbb", "a"))
+print(contar_consecutivos("bAAAAb", "b"))
+print(contar_consecutivos("AAAAAb", "e"))
+print(contar_consecutivos("", "x"))
