87539d4cb28af1e7417849aac8be593d8c21f2a9
padredemellis <10656@holbertonstudents.com>
2026-01-25 11:48:17 -0300

    contador

--- contador-de-palabras/contador.py
+++ contador-de-palabras/contador.py
@@ -2,14 +2,20 @@
 Docstring for contador-de-palabras.contador
 """
 texto: str = "hola mundo hola python mundo mundo"  # string original
+
 texto_a_palabras: list = texto.split()  # convierto la string a lista
+
 print(texto_a_palabras)  # imprimo para ver la lista
+
 frecuencia: dict = {}  # creo un dicc vacio al cual le agregare el contador
+
 for palabra in texto_a_palabras:  # itero la lista
+
     if palabra in frecuencia:  # si la palabra esta en la lista la sumo
         frecuencia[palabra] += 1
     else:
         frecuencia[palabra] = 1  # si no esta en la lista la agrego
+
 print(frecuencia)
 
 mayorOcurrencia: str = ''
@@ -20,4 +26,5 @@ for c, v in frecuencia.items():
     if v > contador:
         contador = v
         mayorOcurrencia = c
-print(f"La palabra que aparece mas veces es '{mayorOcurrencia}' y aparece {contador} veces")
+print(f"La palabra que aparece mas veces es '{mayorOcurrencia}' "
+f"y aparece {contador} veces")
